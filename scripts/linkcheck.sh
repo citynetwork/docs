49eeb6ef8d16b77e28751bcb905b6f053584f580
Florian Haas <florian@cleura.com>
2023-07-05 11:48:38 +0200

    build: Rewrite link checker, make it run on commit
    Prior to this change, the dead links check with linkchecker could only
    be run post-merge. With this change, it checks for dead links on every
    push, as part of the normal CI testing via GitHub Actions.
    
    * Create a wrapper script around `linkchecker` named `linkcheck.sh`,
      and make it behave differently based on whether it's invoked as
      `linkcheck-local.sh` or `linkcheck-production.sh`.
    
    * For `linkcheck-local.sh`, fire up a server on localhost:8000, and run
      `linkchecker` against that. Use that as part of the GitHub Actions
      `build` job, which runs on every push and PR.
    
    * For `linkcheck-production.sh`, run linkchecker against
      https://docs.cleura.cloud. Invoke that from the `check` job, which
      runs on a schedule and can also be invoked manually.
    
    Wrapping everything in a script also means we no longer need a
    configuration file for `linkchecker`, so remove `.linkcheckerrc` from
    the repository.

---
+++ scripts/linkcheck.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -ex
+
+DOCS_LINKCHECK_IGNORE='.*github\.com.*/edit/.*'
+
+if test `basename $0` = "linkcheck-local.sh"; then
+    DOCS_SITE_URL="http://localhost:8000"
+elif test `basename $0` = "linkcheck-production.sh"; then
+    DOCS_SITE_URL="https://docs.cleura.cloud"
+    DOCS_LINKCHECK_IGNORE="$DOCS_LINKCHECK_IGNORE '.*localhost.*'"
+fi
+
+# If invoked as just linkcheck.sh, must pass in DOCS_SITE_URL from the
+# environment.
+if test -z "$DOCS_SITE_URL"; then
+    echo "Must set DOCS_SITE_URL!" >&2
+    exit 1
+fi
+
+# Make sure that the subsequent mkdocs invocation knows the site URL
+export DOCS_SITE_URL
+
+for regex in $DOCS_LINKCHECK_IGNORE; do
+  linkchecker_ignore_options="$linkchecker_ignore_options --ignore $regex"
+done
+
+if test DOCS_SITE_URL="http://localhost:8000"; then
+  # Build the docs, then launch a local Python server in the background.
+  # This ought to be functionally equivalent to running "mkdocs serve",
+  # but it isn't: "mkdocs serve" does not rebuild the sitemap.
+  mkdocs build --strict
+  python -m http.server -b 127.0.0.1 -d site/ \
+    >http.server.out 2>http.server.err &
+  server_pid=$!
+
+  # Make sure we shut down the server on Ctrl-C, or when the script
+  # exits.
+  trap "kill $server_pid" SIGINT EXIT
+
+  # Wait for the server to respond
+  curl -s -I \
+    --retry 60  --retry-connrefused --retry-delay 1 \
+    $DOCS_SITE_URL
+fi
+
+# Actually run the link check
+linkchecker \
+  --check-extern \
+  --recursion-level 2 \
+  $linkchecker_ignore_options \
+  $* \
+  $DOCS_SITE_URL/sitemap.xml 
