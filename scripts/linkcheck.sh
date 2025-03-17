fb0f43bd0895d3e9ee15ffd37c81a6fa77dacd86
Florian Haas <florian.haas@cleura.com>
2025-03-17 14:01:15 +0100

    fix: Update linkcheck.sh for v2
    * Update DOCS_SITE_URL for v2.
    * Exclude sto-com.cleura.cloud to skip checking the API endpoints for
      robots.txt.

--- scripts/linkcheck.sh
+++ scripts/linkcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
-DOCS_LINKCHECK_IGNORE='.*github\.com.*/edit/.*'
+DOCS_LINKCHECK_IGNORE='.*github\.com.*/edit/.* .*sto-com.cleura.cloud.*'
 
 if test `basename $0` = "linkcheck-local.sh"; then
     DOCS_SITE_URL="http://localhost:8000"
 elif test `basename $0` = "linkcheck-production.sh"; then
-    DOCS_SITE_URL="https://docs.cleura.cloud"
+    DOCS_SITE_URL="https://docs-v2.cleura.cloud"
     DOCS_LINKCHECK_IGNORE="$DOCS_LINKCHECK_IGNORE .*localhost.*"
 fi
 
