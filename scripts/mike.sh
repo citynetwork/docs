4a8cccea17bfb335526161171c4f8234fa1e0034
Florian Haas <florian.haas@cleura.com>
2025-08-12 14:19:33 +0200

    build: Title-case branch names for mike versions
    While our per-region branches are all-lowercase in keeping with
    established Git convention ("sto2", "kna1" etc.), our region names use
    title case ("Sto2", "Kna1" etc.). As the region names are more
    meaningful to customers, use the title-case convention for the mike
    version names.
    
    Abuse the aliases functionality for this purpose: make the "version" the
    title-cased one, and use a lowercased "alias", so that people can
    also use an all-lowercase URL.

--- scripts/mike.sh
+++ scripts/mike.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-DOCS_DEPLOY_DEFAULT_BRANCH="${DOCS_DEPLOY_DEFAULT_BRANCH:-main}"
-DOCS_DEPLOY_BRANCH="${DOCS_DEPLOY_BRANCH:-`git rev-parse --abbrev-ref HEAD`}"
+title_case() {
+    echo -n "$@" | sed -e 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g'
+}
 
-mike deploy $DOCS_DEPLOY_BRANCH --allow-empty "$@"
-mike set-default $DOCS_DEPLOY_DEFAULT_BRANCH "$@"
+DOCS_DEPLOY_DEFAULT_BRANCH="${DOCS_DEPLOY_DEFAULT_BRANCH:-main}"
+mike_default_version_name="title_case ${DOCS_DEPLOY_DEFAULT_BRANCH}"
+
+DOCS_DEPLOY_BRANCH="${DOCS_DEPLOY_BRANCH:-`git rev-parse --abbrev-ref HEAD`}"
+mike_version_name=`title_case "${DOCS_DEPLOY_BRANCH}"`
+mike_version_alias="${DOCS_DEPLOY_BRANCH}"
+
+mike deploy \
+     --allow-empty \
+     $mike_version_name \
+     $mike_version_alias \
+     "$@"
+
+mike set-default $mike_default_version_name "$@"
